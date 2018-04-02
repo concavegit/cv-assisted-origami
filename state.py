0573838e04cfc11aa411c9f729830790dcb543a0
Kawin Nikomborirak <concavemail@gmail.com>
2018-04-02 11:21:26 +0000

    Instructions MVP

---
+++ state.py
@@ -0,0 +1,34 @@
+'''
+@author: Kawin Nikomborirak
+@date: 2018/04/02
+@brief: This class has keeps track of the paper state.
+'''
+
+import cv2
+import os
+import re
+
+
+class Instructions:
+    def __init__(self, stepDir):
+        '''Initialize with a path to the directory containg the images
+        sorted in alphabetical order.
+        Each image has edges drawn in black (#000000), with the new
+        fold in red (#ff0000)
+
+        Fields:
+        currentStep: The current step in the instructions, an Int
+        steps: A list of images as described above.
+        '''
+
+        self.currentStep = 0
+
+        files = os.listdir('stepDir')
+        stepRegxp = re.compile('step[0-9]+\.(jpg|png)')
+        stepFiles = list(filter(None, [stepRegxp.match(img) for img in files]))
+        stepFiles = [match.group() for match in stepFiles]
+        self.steps = [cv2.imread(filepath) for filepath in stepFiles]
+
+    def nextStep(self):
+        '''proceed to next step unless all steps are exhausted'''
+        self.currentStep += 1 if self.currentStep < len(self.steps) else 0
