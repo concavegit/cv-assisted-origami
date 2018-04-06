f89fb2fc0dad9ae77735284e9de8c3730d773bf4
Siddharth Garimella <gsidsid@gmail.com>
2018-04-06 13:49:28 -0400

    sift + orb patterns

--- cvo_sandbox_sid_old.py
+++ cvo_sandbox_sid_old.py
@@ -6,12 +6,56 @@ Olin College 2017-2018
 Mark, Kevin, Sid
 """
 
-import cv2
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
-lower_white = np.array([0,0,0], dtype=np.uint8)
-upper_white = np.array([180,255,255], dtype=np.uint8)
+"""
+def detection(frame):
 
+	MIN_MATCH_COUNT = 10
+
+	img1 = cv2.imread('pattern3.jpg',0)          # queryImage
+
+	img2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	orb = cv2.ORB_create()
+	kp1, des1 = orb.detectAndCompute(img1,None)
+	kp2, des2 = orb.detectAndCompute(img2,None)
+	bf = cv2.BFMatcher()
+	matches = bf.knnMatch(des1,des2, k=2)
+	#matches = sorted(matches, key = lambda x:x.distance)
+
+	good = []
+	for m,n in matches:
+	    if m.distance < 0.8*n.distance:
+	        good.append([m])
+
+	img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+
+	return img3
+"""
+"""
+def detection(frame):
+
+	MIN_MATCH_COUNT = 5
+
+	img1 = cv2.imread('checker.jpg',0)          # queryImage
+
+	#img2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	img2 = frame
+
+	orb = cv2.ORB_create()
+	kp1, des1 = orb.detectAndCompute(img1,None)
+	kp2, des2 = orb.detectAndCompute(img2,None)
+	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+	matches = bf.match(des1,des2)
+	matches = sorted(matches, key = lambda x:x.distance)
+
+	img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:MIN_MATCH_COUNT], None, flags=2)
+
+
+	return img3
 
 def detection(img):
 	im = img
@@ -20,7 +64,7 @@ def detection(img):
 	imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 	imhsv = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
 
-	ret,thresh = cv2.threshold(imgray,128,255,0)
+	ret,thresh = cv2.threshold(imgray,127,255,0)
 
 	image, contours, hiers = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	largest = max(contours, key = cv2.contourArea)
@@ -31,16 +75,33 @@ def detection(img):
 	cv2.rectangle(imgc,(x,y),(x+w,y+h),(0,0,255),3)
 	#mask = cv2.inRange(imhsv, lower_white, upper_white)
 
-	blur = cv2.GaussianBlur(img,(5,5),0)
-
+	#blur = cv2.GaussianBlur(img,(5,5),0)
+	 
 	# Set threshold and maxValue
-	threshn = 150
-	maxValue = 245
-
+	thresh = 130	
+	maxValue = 220
+	 
 	# Basic threshold example
-	th, dst = cv2.threshold(img, threshn, maxValue, cv2.THRESH_BINARY);
+	th, dst = cv2.threshold(img, thresh, maxValue, cv2.THRESH_BINARY);
 
-	return thresh
+	return dst
+"""
+
+
+def detection(img):
+	# Image preprocessing and thresholding
+	imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	thresh = imgray.mean()	
+	maxValue = 195
+	th, dst = cv2.threshold(imgray, thresh, maxValue, cv2.THRESH_BINARY);
+
+	# Image contour detection
+	img, cnt, hiers = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	largest - max(cnt, key=cv2.contourArea)
+	imgn = cv2.drawContours(img, largest, -1, (0,255,255), 3)
+
+	return img
+
 
 vc = cv2.VideoCapture(0)
 
@@ -59,3 +120,5 @@ while rval:
 		break
 
 cv2.destroyWindow("feed")
+
+
