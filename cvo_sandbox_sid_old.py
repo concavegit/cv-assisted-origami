0e6020df13b657807fdd13ee981ce790de090746
Siddharth Garimella <gsidsid@gmail.com>
2018-04-13 11:42:09 -0400

    oh hi there

--- cvo_sandbox_sid_old.py
+++ cvo_sandbox_sid_old.py
@@ -76,31 +76,99 @@ def detection(img):
 	#mask = cv2.inRange(imhsv, lower_white, upper_white)
 
 	#blur = cv2.GaussianBlur(img,(5,5),0)
-	 
+
 	# Set threshold and maxValue
-	thresh = 130	
+	thresh = 130
 	maxValue = 220
-	 
+
 	# Basic threshold example
 	th, dst = cv2.threshold(img, thresh, maxValue, cv2.THRESH_BINARY);
 
 	return dst
 """
-
+def around(x, base=5):
+	return int(base * round(float(x)/base))
 
 def detection(img):
 	# Image preprocessing and thresholding
+	canvas = np.zeros((img.shape[0], img.shape[1], 3), dtype = "uint8")
+
 	imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	thresh = imgray.mean()	
-	maxValue = 195
-	th, dst = cv2.threshold(imgray, thresh, maxValue, cv2.THRESH_BINARY);
+	thresh = imgray.mean()
+	maxValue = 180
+	ret, thresh = cv2.threshold(imgray, thresh, maxValue, 0);
 
 	# Image contour detection
-	img, cnt, hiers = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	largest - max(cnt, key=cv2.contourArea)
-	imgn = cv2.drawContours(img, largest, -1, (0,255,255), 3)
+	#img, cnt, hiers = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	#largest = max(cnt, key=cv2.contourArea)
+	#imgn = cv2.drawContours(img, cnt, 2, (0,255,0), 3)
 
-	return img
+	gray = cv2.bilateralFilter(thresh, 9, 17, 17)
+	edges = cv2.Canny(gray, 100, 200, apertureSize = 3)
+	"""
+	corners = cv2.goodFeaturesToTrack(edged, 4, 0.01, 10)
+	corners = np.int0(corners)
+
+	for corner in corners:
+	    x,y = corner.ravel()
+	    cv2.circle(edged,(x,y),3,255,-1)
+
+		im2,contours,hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+		if len(contours) != 0:
+			c = max(contours, key = cv2.contourArea)
+			cv2.drawContours(edges, c, -1, 255, 3)
+
+	"""
+
+	minLineLength = 5
+	maxLineGap = 10
+
+
+	#dst = cv2.cornerHarris(edges,30,17,0.06)
+	#dst = cv2.dilate(dst,None)
+
+	# Threshold for an optimal value, it may vary depending on the image.
+	#canvas[dst>0.5*dst.max()]=[255,255,255]
+
+	lines = cv2.HoughLines(edges,1,np.pi/360,6,minLineLength,maxLineGap)
+
+	# We need to filter these lines somehow to reduce random/arbitrary data
+	theta_bins = dict()
+
+	for line in lines:
+		for rho,theta in line:
+			nearest = str(around(int(theta), base=5))
+			if nearest in theta_bins:
+				theta_bins[nearest] += 1
+			else:
+				theta_bins[nearest] = 1
+
+	common_theta_base = sorted(theta_bins, key=theta_bins.get, reverse=True)
+	print(common_theta_base)
+
+	if len(lines) > 20:
+		for line in lines[:20]:
+			for rho,theta in line:
+				a = np.cos(theta)
+				b = np.sin(theta)
+				x0 = a*rho
+				y0 = b*rho
+				x1 = int(x0 + 1000*(-b))
+				y1 = int(y0 + 1000*(a))
+				x2 = int(x0 - 1000*(-b))
+				y2 = int(y0 - 1000*(a))
+				cv2.line(canvas,(x1,y1),(x2,y2),(255,255,255),2)
+
+
+	return canvas
+
+
+	#dst = cv2.cornerHarris(canvas,10,17,0.06)
+	#dst = cv2.dilate(dst,None)
+
+	# Threshold for an optimal value, it may vary depending on the image.
+	#canvas[dst>0.5*dst.max()]=[0,0,255]
 
 
 vc = cv2.VideoCapture(0)
@@ -120,5 +188,3 @@ while rval:
 		break
 
 cv2.destroyWindow("feed")
-
-
