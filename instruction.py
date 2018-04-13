2c4240f9ef78577e1f69b418b36b8656a4bf391a
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-04-13 13:25:15 -0400

    Made project instructions class

--- instruction.py
+++ instruction.py
@@ -1,13 +1,14 @@
 '''
 @author: Kawin Nikomborirak
 @date: 2018/04/02
-@brief: This class has keeps track of the paper state.
+@brief: These classes has keeps track of the paper state.
 '''
 
 import cv2
 import os
 import re
 import numpy as np
+from PIL import Image
 
 
 class Instruction(object):
@@ -25,7 +26,7 @@ class Instruction(object):
         self.currentStep = 0
 
         files = os.listdir(stepDir)
-        stepRegxp = re.compile('step[0-9]+\.(jpg|png)')
+        stepRegxp = re.compile('step[0-9]+\.(jpg)')
         stepFiles = list(filter(None, [stepRegxp.match(img) for img in files]))
         stepFiles = [match.group() for match in stepFiles]
         self.steps = [cv2.imread(stepDir + '/' + filepath) for filepath in stepFiles]
@@ -39,25 +40,33 @@ class projectInstruction(Instruction):
     # Find centroid AND THEN scale image as well as centroid
     def __init__(self, stepDir):
         super().__init__(stepDir)
-        self.step = self.steps[0]
-        self.cap = cv2.VideoCapture(0)
 
-    def project(self):
-        while(True):
-            # Captures fram-by-frame in "frame" and
-            # whether there is a frame or not (boolean) in ret
-            ret, frame = self.cap.read()
-            # Display the resulting frame, note that the 0xFF is only really needed when numlock is on
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        # When everything is done, release the Capture
-        self.cap.release()
-        cv2.destroyAllWindows()
-
-
-
-
+    def overlayInstructions(self, frame):
+        img = Image.open(self.steps[1])
+        img = img.thumbnail((480,640), Image.ANTIALIAS)
+        rows, cols, channels = img.shape
+        print(rows, cols)
+        roi = frame[0:rows, 0:cols]
+        img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, mask = cv2.threshold(img2gray, 150, 255, cv2.THRESH_BINARY)
+        mask_inv = cv2.bitwise_not(mask)
+        cv2.imshow("mask", mask_inv)
+        img_b = cv2.bitwise_and(roi, roi, mask = mask_inv)
+        return frame
+        
 i = projectInstruction('sample')
-i.project()
-#cv2.imshow("help",i.steps[0])
+#cv2.imshow("help", i.steps[1])
+print(len(i.steps))
+
+cap = cv2.VideoCapture(0)
+while(True):
+    # Captures fram-by-frame in "frame" and
+    # whether there is a frame or not (boolean) in ret
+    ret, frame = cap.read()
+    frame = i.overlayInstructions(frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything is done, release the Capture
+cap.release()
+cv2.destroyAllWindows()
