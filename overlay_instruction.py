519b3c1163062b04ce0f864fcf531c59f653381a
Kawin Nikomborirak <concavemail@gmail.com>
2018-04-14 15:28:01 +0000

    mvp

--- overlay_instruction.py
+++ overlay_instruction.py
@@ -9,6 +9,13 @@ import cv2
 
 class OverlayInstruction:
     def __init__(self, real, instruction):
+        '''
+        Fields:
+        - instruction: full color of instruction image
+        - outline: grayscale instruction outline
+        - verts: vertices of instruction outline
+        - realVerts: vertices of real camera input
+        '''
         self.alphaToWhite(instruction)
 
         gray = cv2.cvtColor(self.instruction, cv2.COLOR_BGR2GRAY)
@@ -16,13 +23,27 @@ class OverlayInstruction:
         # Just the outline of the shape
         self.outline = np.uint8((gray > 0)) * 255
 
-        # An array of vertices
-        self.vertices = cv2.goodFeaturesToTrack(
-            self.outline, 100, 0.1, 10)[:, 0, :]
+        # An array of verts
+        self.verts = cv2.goodFeaturesToTrack(
+            self.outline, 100, 0.5, 10)[:, 0]
+        self.verts = self.sortVerts(self.verts)
+        self.area = self.computeArea(self.verts)
+
+        self.real = real
+        realGray = cv2.cvtColor(self.real, cv2.COLOR_BGR2GRAY)
+        self.th = cv2.threshold(realGray, 128, 255, cv2.THRESH_BINARY)[1]
+
+        # array of real verts
+        self.realVerts = cv2.goodFeaturesToTrack(self.th, 100, .5, 10)[:, -1]
+        self.realVerts = self.sortVerts(self.realVerts)
+        self.realArea = self.computeArea(self.realVerts)
+
+        self.resizeInstruction()
+        self.overlayInstructions()
 
     def alphaToWhite(self, instruction):
         '''
-        @brief: Convert the transparent instruction background to white.
+        Convert the transparent instruction background to white.
         '''
 
         img = cv2.imread(instruction, -1)
@@ -34,8 +55,85 @@ class OverlayInstruction:
         # Color representation of instruction
         self.instruction = whiteBack
 
-    def sortVertices(self):
-        n = self.vertices.shape[0]
-        centroid = self.vertices.mean(0)
-        d = self.vertices - centroid
-        cornersWith
+    def sortVerts(self, verts):
+        '''
+        Sort the verts in clockwise order.
+        '''
+        centroid = verts.mean(0)
+        d = verts - centroid
+        an = (np.arctan2(d[:, 0], d[:, 1]) + 2 * np.pi) % (2 * np.pi)
+        cornersWithAngles = np.hstack((verts, an.reshape(-1, 1)))
+        x = cornersWithAngles.tolist()
+        x.sort(key=lambda point: point[-1])
+        return np.array(x)[:, :-1]
+
+    def computeArea(self, verts):
+        '''
+        Use shoelace formula to calculate area
+        '''
+        rolled = np.roll(verts, -1, 0)
+        shoelace = verts[:, 0] * rolled[:, 1]\
+            - verts[:, 1] * rolled[:, 0]
+        return np.abs(shoelace.sum()) / 2
+
+    def resizeInstruction(self):
+        '''
+        Resize the instruction image to match the area of the real image
+        '''
+        scale = np.sqrt(self.realArea / self.area)
+        dim = np.int0(scale * np.float64(self.instruction.shape))[:-1]
+        self.scale = scale
+        self.resized = cv2.resize(self.instruction, (dim[1], dim[0]))
+
+    def overlayInstructions(self):
+        '''
+        Draw the rest of the owl
+        '''
+        realCentroid = self.realVerts.mean(0)
+        centroid = self.verts.mean(0) * self.scale
+        shift = np.int0(realCentroid - centroid)
+        shiftNonNegative = shift.copy()
+        shiftNonNegative[shiftNonNegative < 0] = 0
+
+        # pad the instruction according to the real image
+        topPad = shift[0]
+        bottomPad = self.real.shape[0] - self.resized.shape[0] - topPad
+        leftPad = shift[1]
+        rightPad = self.real.shape[1] - self.resized.shape[1] - leftPad
+        padding = np.reshape(
+            [topPad, bottomPad, leftPad, rightPad, 0, 0], (-1, 2))
+        nonNegPadding = padding.copy()
+        nonNegPadding[nonNegPadding < 0] = 0
+
+        padded = np.pad(self.resized, nonNegPadding,
+                        'constant', constant_values=255)
+
+        # trim the padded image according to the real image
+        trim = padding.copy()[:-1]
+        trim[trim > 0] = 0
+        trim[:, 0] = -trim[:, 0]
+        bottomTrim = trim[0, 1]
+
+        # if the trim on the side is zero, do not cut from the beginning
+        rightTrim = trim[1, 1]
+        bottomTrim = bottomTrim if bottomTrim < 0 \
+            else padded.shape[0] - trim[0, 0]
+        rightTrim = rightTrim if rightTrim < 0 \
+            else padded.shape[1] - trim[1, 0]
+        trimmed = padded[trim[0, 0]:bottomTrim, trim[1, 0]:rightTrim]
+
+        gray = cv2.cvtColor(trimmed, cv2.COLOR_BGR2GRAY)
+        mask = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)[1]
+        invMask = np.bitwise_not(mask)
+        bg = cv2.bitwise_and(self.real, self.real, mask=mask)
+        fg = cv2.bitwise_and(trimmed, trimmed, mask=invMask)
+        overlayed = cv2.add(bg, fg)
+        self.overlayed = overlayed
+
+
+if __name__ == '__main__':
+    r = cv2.imread('PaperPics/triangle.jpg')
+    x = OverlayInstruction(r, 'CompGenInstructions/step02.png')
+    cv2.imshow('y', x.overlayed)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
