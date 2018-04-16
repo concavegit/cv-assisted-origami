7e7277d17b87ca8942a894d07807015fc4a7f64d
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-04-16 19:04:20 -0400

    Speech of instructions partially implemented

--- instruction.py
+++ instruction.py
@@ -8,39 +8,43 @@ import cv2
 import os
 import re
 import numpy as np
-
+import pyttsx3
 
 class Instruction(object):
     def __init__(self, stepDir):
         '''Initialize with a path to the directory containg the images
         named step%d.(png|jpg).
-        Each image has edges drawn in black (#000000), with the new
-        fold in red (#ff0000)
-
-        Fields:
-        currentStep: The current step in the instructions, an Int
-        steps: A list of images as described above.
         '''
+        self.finished = False
 
         self.stepOrder = [1, 0, 2, 4, 3]
+        self.stepToSpeech = {1 : "Fold in half", 0 : "Fold in on the dotted line", 2 : "Fold in on the dotted line", 4 : "Turn over", 3 : "Draw eyes and a nose"}
         self.stepCounter = 0
         self.currentStep = self.stepOrder[self.stepCounter]
+
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate', 150)
 
         files = os.listdir(stepDir)
         stepRegxp = re.compile('Step[1-5]+\.(jpg)')
         stepFiles = list(filter(None, [stepRegxp.match(img) for img in files]))
         stepFiles = [match.group() for match in stepFiles]
         self.steps = [cv2.imread(stepDir + '/' + filepath) for filepath in stepFiles]
+        print(len(self.steps))
 
     def nextStep(self):
         '''Proceed to next step unless all steps are exhausted'''
-        if self.currentStep < len(self.steps):
+        self.said = True
+        if self.stepCounter < len(self.steps) - 1:
             self.stepCounter += 1
         else:
+            self.finished = True
             self.stepCounter = 0
         self.currentStep = self.stepOrder[self.stepCounter]
+        self.engine.say(self.stepToSpeech[self.stepOrder[self.stepCounter]])
+        self.engine.runAndWait()
 
-class projectInstruction(Instruction):
+class InstructUser(Instruction):
     """ This class will fetch an instruction and project it into the upper left corner or into the paper
     (currently only does upper left)"""
 
@@ -60,6 +64,8 @@ class projectInstruction(Instruction):
         return frame
 
     def projectOntoVideo(self):
+        """Overlays a jpg of origami instructions onto the frame"""
+
         # Captures fram-by-frame in "frame" and
         # whether there is a frame or not (boolean) in ret
         ret, frame = self.cap.read()
@@ -68,13 +74,10 @@ class projectInstruction(Instruction):
         cv2.imshow('frame', frame)
 
 
-
 if __name__ == "__main__":
-    i = projectInstruction('OrigamiFox')
-    while True:
+    i = InstructUser('OrigamiFox')
+    while not i.finished:
         i.projectOntoVideo()
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
         if cv2.waitKey(1) & 0xFF == ord('k'):
             i.nextStep()
     # When everything is done, release the Capture
