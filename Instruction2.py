2c166fc3067ce488f052198d0ac8fe670f0d3a60
Kawin Nikomborirak <concavemail@gmail.com>
2018-04-17 13:29:46 +0000

    prepare to overlay onto video

---
+++ Instruction2.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+import re
+import numpy as np
+
+
+class Instruction(object):
+    def __init__(self, stepDir):
+        '''Initialize with a path to the directory containg the images
+        named step%d.(png|jpg).
+        Each image has edges drawn in black (#000000), with the new
+        fold in red (#ff0000)
+
+        Fields:
+        currentStep: The current step in the instructions, an Int
+        steps: A list of images as described above.
+        '''
+
+        self.currentStep = 0
+
+        files = sorted(os.listdir(stepDir))
+        stepRegxp = re.compile('step[0-9]+\.png')
+        stepFiles = list(filter(None, [stepRegxp.match(img) for img in files]))
+        stepFiles = [match.group() for match in stepFiles]
+        self.steps = [self.alphaToWhite(cv2.imread(stepDir + '/' + filepath, -1))
+                      for filepath in stepFiles]
+
+    def nextStep(self):
+        '''Proceed to next step unless all steps are exhausted'''
+        self.currentStep += 1 if self.currentStep < len(self.steps) else 0
+
+    def alphaToWhite(self, instruction):
+        alpha = instruction[:, :, -1]
+        mask = np.int8((alpha == 0) * 255)
+        color = instruction[:, :, :-1]
+        whiteBack = cv2.bitwise_not(color, np.array(color), mask=mask)
+
+        # Color representation of instruction
+        return whiteBack
--- instruction.py
+++ instruction.py
@@ -25,14 +25,16 @@ class Instruction(object):
         self.currentStep = 0
 
         files = os.listdir(stepDir)
-        stepRegxp = re.compile('step[0-9]+\.(jpg)')
+        stepRegxp = re.compile('step[0-9]+\.(png)')
         stepFiles = list(filter(None, [stepRegxp.match(img) for img in files]))
         stepFiles = [match.group() for match in stepFiles]
-        self.steps = [cv2.imread(stepDir + '/' + filepath) for filepath in stepFiles]
+        self.steps = [cv2.imread(stepDir + '/' + filepath)
+                      for filepath in stepFiles]
 
     def nextStep(self):
         '''Proceed to next step unless all steps are exhausted'''
         self.currentStep += 1 if self.currentStep < len(self.steps) else 0
+
 
 class projectInstruction(Instruction):
     """ This class will fetch an instruction and project it into the upper left corner or into the paper
@@ -49,20 +51,23 @@ class projectInstruction(Instruction):
         img = self.steps[1]
         # scales image down by about a third
         res = cv2.resize(img, None, fx=0.3, fy=0.3)
-        frame[0:res.shape[0] - 130, 0:res.shape[1] - 30] = res[40:res.shape[0] - 90, 20:res.shape[1] - 10]
+        frame[0:res.shape[0] - 130, 0:res.shape[1] -
+              30] = res[40:res.shape[0] - 90, 20:res.shape[1] - 10]
         return frame
 
-i = projectInstruction('CompGenInstructions')
-cap = cv2.VideoCapture(0)
-while(True):
-    # Captures fram-by-frame in "frame" and
-    # whether there is a frame or not (boolean) in ret
-    ret, frame = cap.read()
-    frame = i.overlayInstructions(frame)
-    #frame = i.overlayInstructions(frame)
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-# When everything is done, release the Capture
-cap.release()
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    i = projectInstruction('CompGenInstructions')
+    cap = cv2.VideoCapture(0)
+    while(True):
+        # Captures fram-by-frame in "frame" and
+        # whether there is a frame or not (boolean) in ret
+        ret, frame = cap.read()
+        frame = i.overlayInstructions(frame)
+        #frame = i.overlayInstructions(frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # When everything is done, release the Capture
+    cap.release()
+    cv2.destroyAllWindows()
