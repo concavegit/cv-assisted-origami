c744dc19e87fdffa2a3a41c96e61683fb411e0b7
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-04-19 21:39:43 -0400

    Ready to integrate

--- instruction.py
+++ instruction.py
@@ -9,27 +9,25 @@ import os
 import re
 import numpy as np
 import pyttsx3
-import time
 
 
 class Instruction(object):
     def __init__(self, stepDir):
         '''Initialize with a path to the directory containg the images
-        named step%d.(png|jpg).
+        named step%d.(jpg).
         '''
         self.finished = False
         self.said = False
 
-        self.stepOrder = [1, 0, 2, 4, 3]
-        self.stepToSpeech = {1: "Fold in half", 0: "Fold in on the dotted line",
-                             2: "Fold in on the dotted line", 4: "Turn over", 3: "Draw eyes and a nose"}
+        self.stepToSpeech = {0: "Fold in half", 1: "Fold in on the dotted line", 2: "Fold in on the dotted line", 3: "Turn over", 4: "Draw eyes and a nose"}
         self.stepCounter = 0
-        self.currentStep = self.stepOrder[self.stepCounter]
 
         self.engine = pyttsx3.init()
         self.engine.setProperty('rate', 115)
 
-        files = os.listdir(stepDir)
+        self.skipCounter = 0
+
+        files = sorted(os.listdir(stepDir))
         stepRegxp = re.compile('Step[1-5]+\.(jpg)')
         stepFiles = list(filter(None, [stepRegxp.match(img) for img in files]))
         stepFiles = [match.group() for match in stepFiles]
@@ -37,13 +35,15 @@ class Instruction(object):
 
     def nextStep(self):
         '''Proceed to next step unless all steps are exhausted'''
+        self.skipCounter = 0
+
         self.said = True
         if self.stepCounter < len(self.steps) - 1:
             self.stepCounter += 1
         else:
             self.finished = True
             self.stepCounter = 0
-        self.currentStep = self.stepOrder[self.stepCounter]
+        self.currentStep = self.steps[self.stepCounter]
         self.said = False
 
 
@@ -60,7 +60,7 @@ class InstructUser(Instruction):
         """Instruction mode 1: Instructinos displayed in the upper
         left corner"""
 
-        img = self.steps[self.currentStep]
+        img = self.steps[self.stepCounter]
         # scales image down by about a third
         res = cv2.resize(img, None, fx=0.7, fy=0.7)
         frame[0:res.shape[0], 0:res.shape[1]
@@ -69,7 +69,8 @@ class InstructUser(Instruction):
 
     def projectOntoVideo(self):
         """Overlays a jpg of origami instructions onto the frame"""
-
+        if self.said == False:
+            self.skipCounter += 1
         # Captures fram-by-frame in "frame" and
         # whether there is a frame or not (boolean) in ret
         ret, frame = self.cap.read()
@@ -77,13 +78,15 @@ class InstructUser(Instruction):
         #frame = i.overlayInstructions(frame)
 
         cv2.imshow('frame', frame)
-        if not self.said:
+
+
+        if not self.said and self.skipCounter > 2:
             self.stateInstructions()
 
     def stateInstructions(self):
         """States instructions at current step"""
 
-        self.engine.say(self.stepToSpeech[self.stepOrder[self.stepCounter]])
+        self.engine.say(self.stepToSpeech[self.stepCounter])
         self.engine.runAndWait()
         self.said = True
 
