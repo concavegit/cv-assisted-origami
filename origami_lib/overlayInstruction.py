e2cf5851b358cd79c259de2de65dd0c72c5b75fa
Kawin Nikomborirak <concavemail@gmail.com>
2018-05-08 15:00:38 +0000

    better docstrings for overlayInstructions :smile:

--- origami_lib/overlayInstruction.py
+++ origami_lib/overlayInstruction.py
@@ -8,7 +8,19 @@ from .InstructionPaper import Instruction
 
 
 def overlayInstruction(real, instruction):
-    '''Overlay the instructions onto real camera input'''
+    '''Overlay the instructions onto real camera input
+
+    Parameters:
+    real: np.ndarray (shape: (,,3))
+      An image of camera input
+    instruction: np.ndarray (shape: (,,3))
+      An image of the instructions.
+
+    Returns:
+    np.ndarray (shape: (,,3))
+      An image with the instructions overlayed onto the camera input.
+    '''
+
     gray = cv2.cvtColor(instruction, cv2.COLOR_BGR2GRAY)
     outline = np.uint8(gray > 8) * 255
     verts = cv2.goodFeaturesToTrack(outline, 100, 0.5, 10)[:, 0]
@@ -66,7 +78,17 @@ def overlayInstruction(real, instruction):
 
 
 def sortVerts(verts):
-    '''Sort vertices in clockwise order.'''
+    '''Sort vertices in clockwise order
+
+    Parameters:
+    verts: np.ndarray (shape: (-1,2))
+      Coordinates of vertices.
+
+    Returns:
+    np.ndarray (shape: (-1, 2))
+      Vertices sorted in clockwise order.
+    '''
+
     centroid = verts.mean(0)
     d = verts - centroid
     an = (np.arctan2(d[:, 0], d[:, 1]) + 2 * np.pi) % (2 * np.pi)
@@ -76,7 +98,16 @@ def sortVerts(verts):
 
 
 def computeArea(verts):
-    '''Compute area using the shoelace formula.'''
+    '''Compute area using the shoelace formula.
+
+    Parameters:
+    verts: np.ndarray (shape: (-1,2))
+      Coordinates of vertices.
+
+    Returns:
+    A float for the area of the polygon traced by the vertices.
+    '''
+
     verts = sortVerts(verts)
     rolled = np.roll(verts, -1, 0)
     shoelace = verts[:, 0] * rolled[:, 1]\
@@ -85,13 +116,32 @@ def computeArea(verts):
 
 
 def resizeInstruction(instruction, scale):
-    '''Resize the instruction image based on a scale.'''
+    '''Resize the instruction image based on a scale.
+    Parameters:
+    instruction: np.ndarray (shape: (,,3))
+      An image of the instructions.
+    scale: float
+      A scale factor.
+
+    Returns:
+    np.ndarray (shape: (,,3))
+     An scaled image.
+    '''
     dim = np.int0(scale * np.float64(instruction.shape))[:-1]
     return cv2.resize(instruction, (dim[1], dim[0]))
 
 
 def overlayVideo(cap, directory):
-    '''Overlay the instructions on a video'''
+    '''Overlay the instructions on a video
+    Parameters:
+    cap: cv2.VideoCapture
+      A source from which to get camera input.
+    directory: string
+      A string pointing to a directory of instruction images.
+
+    Returns:
+    NoneType
+    '''
     x = Instruction(directory)
     while x.running:
         ret, frame = cap.read()
@@ -105,7 +155,14 @@ def overlayVideo(cap, directory):
 
 
 def run(directory):
-    '''Run the overlayVideo on a video device.'''
+    '''Run the overlayVideo on a video device.
+    Parameters:
+    directory: string
+      A string pointing to a directory of instruction images.
+
+    Returns:
+    NoneType
+    '''
     try:
         cap = cv2.VideoCapture(1)
         overlayVideo(cap, directory)
